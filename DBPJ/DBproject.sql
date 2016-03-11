af78d399986538c11b52085319cb02c1b6b4f2a0
KingOfPoutine <rogerliuray@gmail.com>
2016-03-11 16:28:22 -0500

    USER
    I added user table and set up some constraints to verify the password

--- DBPJ/DBproject.sql
+++ DBPJ/DBproject.sql
@@ -4,6 +4,33 @@ CREATE DATABASE CSI2132PJ;
 
 CREATE SCHEMA MOVEDB;
 
-CREATE TABLE USER(
-  UserID 
-)
+CREATE TABLE USERS(
+  UserID SERIAL PRIMARY KEY,
+  password CHAR(20) NOT NULL,
+  Last_Name CHAR(20) NOT NULL,
+  First_Name CHAR(20) NOT NULL,
+  Email CHAR(40) NOT NULL UNIQUE,
+  City CHAR(40),
+  Province CHAR(40),
+  Country CHAR(40),
+
+
+);
+
+CREATE FUNCTION  check_password()
+  RETURNS trigger AS
+  $BODY$
+  BEGIN
+  IF
+  NEW.password LIKE '%123%' THEN
+     RAISE EXCEPTION 'Sorry the password you entered is too simple.';
+  END IF;
+  RETURN NEW;
+  END
+  $BODY$ LANGUAGE plpgsql;
+
+
+  CREATE TRIGER check_password
+  BEFORE INSERT OR UPDATE
+  ON USER
+  EXECUTE PROCEDURE check_password();
