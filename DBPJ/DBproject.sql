700e559a8a5545505e8d94971c5448be124b3a92
KingOfPoutine <rogerliuray@gmail.com>
2016-03-11 16:29:22 -0500

    Test

--- DBPJ/DBproject.sql
+++ DBPJ/DBproject.sql
@@ -3,6 +3,9 @@
 CREATE DATABASE CSI2132PJ;
 
 CREATE SCHEMA MOVEDB;
+SET SEARCH_PATH='movedb';
+
+DROP TABLE USERS;
 
 CREATE TABLE USERS(
   UserID SERIAL PRIMARY KEY,
@@ -12,10 +15,16 @@ CREATE TABLE USERS(
   Email CHAR(40) NOT NULL UNIQUE,
   City CHAR(40),
   Province CHAR(40),
-  Country CHAR(40),
-
-
+  Country CHAR(40)
 );
+
+SELECT * FROM USERS;
+
+INSERT INTO USERS("password","last_name","first_name","email","city","province","country")
+VALUES('214','Liu','Ray','rogerliuray@gmail.com','nanjing','jiangsu','China');
+INSERT INTO USERS("password","last_name","first_name","email","city","province","country")
+VALUES('321','Liu','fei','fei@gmail.com','nanjing','jiangsu','China');
+
 
 CREATE FUNCTION  check_password()
   RETURNS trigger AS
@@ -29,8 +38,8 @@ CREATE FUNCTION  check_password()
   END
   $BODY$ LANGUAGE plpgsql;
 
-
-  CREATE TRIGER check_password
+ CREATE TRIGGER check_password
   BEFORE INSERT OR UPDATE
-  ON USER
+  ON USERS
+  FOR EACH ROW
   EXECUTE PROCEDURE check_password();
