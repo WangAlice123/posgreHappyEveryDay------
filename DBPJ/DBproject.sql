0745ad57d1cb8a4acd8131e5fb7f60f3473aa81b
KingOfPoutine <rogerliuray@gmail.com>
2016-03-12 19:25:18 -0500

    Five tables added

--- DBPJ/DBproject.sql
+++ DBPJ/DBproject.sql
@@ -6,7 +6,7 @@ CREATE SCHEMA MOVEDB;
 SET SEARCH_PATH='movedb';
 
 DROP TABLE USERS;
-
+/*Users*****************************************************/
 CREATE TABLE USERS(
   UserID SERIAL PRIMARY KEY,
   password CHAR(20) NOT NULL,
@@ -25,6 +25,24 @@ VALUES('214','Liu','Ray','rogerliuray@gmail.com','nanjing','jiangsu','China');
 INSERT INTO USERS("password","last_name","first_name","email","city","province","country")
 VALUES('321','Liu','fei','fei@gmail.com','nanjing','jiangsu','China');
 
+/*Profile************************************/
+CREATE TABLE PROFILE(
+     userid INT NOT NULL,
+  CONSTRAINT profile_fkey FOREIGN KEY (userid)
+    REFERENCES USERS (userid)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT profile_pkey PRIMARY KEY (userid,year_born),
+    age_range char(20) CHECK(age_range IN('Youth','Adult','Elderly')),
+    year_born char(4),
+    gender    char(20) CHECK(gender IN('Male','Female','Unknown')),
+    occupation char(40),
+    device_used char(40));
+
+SELECT * FROM PROFILE;
+INSERT INTO profile
+VALUES(1,'Adult','1994','Male','Software Engineer','Mac book pro');
+INSERT INTO profile
+VALUES(2,'Adult','1995','Male','Software Engineer','Mac book pro');
 
 CREATE FUNCTION  check_password()
   RETURNS trigger AS
@@ -43,3 +61,34 @@ CREATE FUNCTION  check_password()
   ON USERS
   FOR EACH ROW
   EXECUTE PROCEDURE check_password();
+
+  /*Topics******************************/
+CREATE TABLE TOPICS(
+  topic_id serial PRIMARY key,
+  description char(40)
+);
+
+
+/*MOVIES******************************/
+
+CREATE TABLE MOVIE(
+  movie_id serial PRIMARY key,
+  name char(50) NOT NULL UNIQUE,
+  date_released DATE NOT NULL,
+
+);
+
+/*WATCHED*****************************/
+CREATE TABLE WATCHES(
+  userid INT NOT NULL,
+  movie_id INT NOT NULL,
+  CONSTRAINT userid_fkey FOREIGN KEY(userid)
+  REFERENCES USERS (userid),
+  CONSTRAINT movie_id FOREIGN KEY(movie_id)
+  REFERENCES MOVIE(movie_id),
+  language char(30),
+  subtitles boolean,
+  country char(40)
+
+
+);
